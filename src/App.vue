ba5613c1f2bb73ee9c5eb4b5d06c5de6a82cf6e1
Anna Gorzanowska <anna.gorzanowska@gmail.com>
2021-05-29 11:28:58 +0200

    change logic to vue

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,17 @@
 <template>
   <div id="app">
-    <div id="intro">
-      <h1>Witaj w systemie zapisów na zajęcia</h1>
+    <h1>Witaj w systemie do zapisów na zajęcia</h1>
+    <div id="info" v-if="authenticated">
+      <h1>Witaj {{ email2 }} !</h1>
+      <button @click="logout()">Wyloguj</button>
+    </div>
+    <div id="intro" v-else>
       <div>
-        Zaloguj się e-mailem <input type="email" v-model="email2" style="margin-right: 2px"/>
-        <button @click="loginByEmail()">Wchodzę </button>
+        Zaloguj się e-mailem
+        <input type="email" v-model="email2" style="margin-right: 2px" />
+        <button @click="loginByEmail()">Wchodzę</button>
       </div>
     </div>
-    <div id="info" style="display: none">
-      <h1>Witaj {{ email2 }} !</h1>
-      <button @click="quit()">Wyloguj </button>
-    </div>
-
-    <!-- <h1>Twój e-mail to {{ email }}</h1>
-    <input type="email" v-model="email" />
-    <div v-if="email.length < 10">Ale masz krótki adres!</div>
-    <div v-else-if="email.length < 15">Twój adres e-mail jest w sam raz.</div>
-    <div v-else>Twój adres e-mail jest stanowczo za długi.</div>
-    <button @click="alertMyEmail()">Wyświetl mój e-mail w alercie</button> -->
   </div>
 </template>
 
@@ -25,23 +19,17 @@
 export default {
   data() {
     return {
-      email: "anna.gorzanowska@gmail.com",
       email2: "",
-      password: "",
+      authenticated: false,
     };
   },
   methods: {
-    alertMyEmail() {
-      alert(this.email);
-    },
     loginByEmail() {
-      document.querySelector("#intro").style = "display: none";
-      document.querySelector("#info").style = "display: block";
+      this.authenticated = true;
     },
-    quit() {
-      document.querySelector("#info").style = "display: none";
-      document.querySelector("#intro").style = "display: block";
-    }
+    logout() {
+      this.authenticated = false;
+    },
   },
 };
 </script>
